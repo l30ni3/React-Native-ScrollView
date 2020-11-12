3dac3fb0d25f748e9d3b2db82a8905c91b820df8
Leonie Koch <lk.leonie.koch@gmail.com>
2020-11-12 20:41:42 +0100

    Added modal component

--- components/Item.jsx
+++ components/Item.jsx
@@ -1,34 +1,55 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import moment from "moment";
+import ModalView from "./Modal";
 
 export const Item = (props) => {
   const { title, timeToComplete, backgroundColor } = props;
 
   const time = moment(new Date(timeToComplete)).format("mm:ss");
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    try {
+      // React Hook: Toggle Modal
+      setModalVisible((modalVisible) => !modalVisible);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <View style={[styles.slide, { backgroundColor: backgroundColor }]}>
-      <Text style={{ ...styles.slideTitle }}>{title}</Text>
-      <Text style={{ ...styles.slideTime }}>{time} Min </Text>
-    </View>
+    <TouchableOpacity
+      activeOpacity={1}
+      style={[styles.touchable, { backgroundColor: backgroundColor }]}
+      onPress={() => toggleModal()}
+    >
+      <View style={[styles.slide, { backgroundColor: backgroundColor }]}>
+        <Text style={{ ...styles.slideTitle }}>{title}</Text>
+        <Text style={{ ...styles.slideTime }}>{time} Min </Text>
+      </View>
+      <ModalView visible={modalVisible} toggle={toggleModal}></ModalView>
+    </TouchableOpacity>
   );
 };
 
 export default Item;
 
 const styles = StyleSheet.create({
+  touchable: {
+    flex: 1,
+    height: 150,
+    borderRadius: 8,
+    marginRight: 16,
+  },
   slide: {
     padding: 15,
-    flex: 1,
-    // justifyContent: "center",
     height: 150,
     borderRadius: 8,
     marginRight: 16,
   },
   slideTitle: {
-    flex: 1,
+    flex: 3,
     width: "80%",
     textAlign: "left",
     color: "rgb(255, 255, 255)",
@@ -43,5 +64,6 @@ const styles = StyleSheet.create({
     color: "rgb(255, 255, 255)",
     fontSize: 16.875,
     lineHeight: 20.625,
+    justifyContent: "flex-end",
   },
 });
